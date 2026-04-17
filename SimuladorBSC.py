b1bf67c060ff509259e1d4e1b6f29c820e47d10e
Alejandroorantes <68298513+Alejandroorantes@users.noreply.github.com>
2026-04-17 14:08:08 -0600

    Fix syntax error in campos_canvas list

--- SimuladorBSC.py
+++ SimuladorBSC.py
@@ -77,7 +77,7 @@ if st.session_state.paso == 1:
         st.subheader("I. Arquitectura de Negocio (Canvas) y Autoevaluación")
         st.caption("Define cada elemento y selecciona si representa actualmente una Fortaleza o una Debilidad.")
 
-        campos_canvas = ["Propuesta de Valor", "Procesos/actividades Clave1","Procesos/Actividades Clave2","Procesos/Actividades Clave3", "Procesos/Actividades Clave4", "Recursos Tecnologico Clave","Recursos Humano Clave", "Recursos Informatico clave" "Infraestructura clave", "Socios Clave"]
+        campos_canvas = ["Propuesta de Valor", "Procesos/actividades Clave1","Procesos/Actividades Clave2","Procesos/Actividades Clave3", "Procesos/Actividades Clave4", "Recursos Tecnologico Clave","Recursos Humano Clave", "Recursos Informatico clave", "Infraestructura clave", "Socios Clave"]
         respuestas_canvas = {}
 
         for campo in campos_canvas:
